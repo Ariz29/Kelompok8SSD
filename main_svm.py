5f52683b01a75b993a597abc5b68063118919f91
Ariz29 <92160976+Ariz29@users.noreply.github.com>
2024-06-10 22:28:56 +0700

    Update main_svm.py

--- main_svm.py
+++ main_svm.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import RobustScaler
+#from sklearn.preprocessing import RobustScaler
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
