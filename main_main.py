630c9da7b537e7d27f6ced89300758f8c9e93edd
Ariz29 <92160976+Ariz29@users.noreply.github.com>
2024-06-11 13:48:35 +0700

    Update main_main.py
    Signed-off-by: Ariz29 <92160976+Ariz29@users.noreply.github.com>

--- main_main.py
+++ main_main.py
@@ -14,3 +14,48 @@ st.title('Nepal Earthquake Severity Classifier')
 
 # Membagi kolom
 col1, col2 = st.columns(2)
+with col1:
+    Hazard = st.text_input('Input Tingkat Resiko Bahaya Gempa')
+with col2:
+    Exposure = st.text_input('Input Tingkat Akurasi Bahaya Gempa')
+with col1:
+    Housing = st.text_input('Input Tingkat Kerusakan Rumah Korban Gempa')
+with col2:
+    Poverty = st.text_input('Input Tingkat Kemiskinan Korban Gempa')
+with col1:
+    Vulnerability = st.text_input('Input Tingkat Bahaya Titik Gempa')
+with col2:
+    Severity = st.text_input('Input Tingkat Keparahan Korban Luka-luka')
+with col1:
+    Severity Normalized = st.text_input('Input Tingkat Keparahan Korban Meninggal')
+
+# Code untuk prediksi
+diab_prediksi = ''
+
+# Membuat tombol untuk prediksi
+if st.button('Test Prediksi Gempa Bumi'):
+    try:
+        # Pastikan input dikonversi ke tipe data yang sesuai
+        input_data = np.array([[int(Hazard), int(Exposure), int(Housing),
+                                int(Poverty), int(Vulnerability), float(Severity),
+                                int(Severity Normalized)]])
+
+        # Terapkan scaler hanya pada fitur yang sesuai
+        features_to_scale = input_data[:, :7]
+        scaled_features = scaler.transform(features_to_scale)
+
+        # Gabungkan fitur yang sudah di-scaling dengan fitur yang tidak di-scaling
+        combined_input_data = np.hstack((scaled_features, input_data[:, 7:]))
+
+        # Lakukan prediksi
+        diab_prediction = earthquake_model.predict(combined_input_data)
+
+        # Pastikan hasil prediksi diubah menjadi nilai tunggal sebelum dibandingkan
+        if (diab_prediction[0] == [1, 0]).all():
+            diab_prediksi = 'Pasien Terkena Diabetes'
+        else:
+            diab_prediksi = 'Pasien Tidak Terkena Diabetes'
+
+        st.success(diab_prediksi)
+    except ValueError as e:
+        st.error(f'Error dalam konversi input: {e}')
