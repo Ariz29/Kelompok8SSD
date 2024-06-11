751b47a93b2289963a53e66b3ef8c1c8ddc2789b
Ariz29 <92160976+Ariz29@users.noreply.github.com>
2024-06-11 13:35:21 +0700

    Update main_main.py
    Signed-off-by: Ariz29 <92160976+Ariz29@users.noreply.github.com>

--- main_main.py
+++ main_main.py
@@ -1,39 +1,16 @@
 import streamlit as st
-import pandas as pd
 import pickle
-import os
+import streamlit as st
+import numpy as np
 
-current_directory = os.path.dirname(__file__)
-file_path = os.path.join(current_directory, 'svm_model.pkl')
-    
-with open(file_path, 'rb') as f:
-# Proses pembacaan file
-except FileNotFoundError as e:
-print(f"File not found: {e}")
-# Tindakan tambahan jika diperlukan, seperti log kesalahan atau fallback logic
+# Membaca model
+earthquake_model = pickle.load(open('gempa_bumi_model.sav', 'rb'))
 
-
-svm_model = pickle.load(f)
+# Membaca scaler
+scaler = pickle.load(open('Robust.pkl', 'rb'))
 
 # Streamlit app
 st.title('Nepal Earthquake Severity Classifier')
 
-# Sidebar untuk input data
-st.sidebar.header('Input Data')
-
-# Menerima input dari pengguna
-input_data = {}
-for feature in features:
-    input_data[feature] = st.sidebar.number_input(f'{feature}', value=0.0)
-
-# Predict button
-if st.sidebar.button('Predict'):
-    # Prepare data for prediction
-    input_df = pd.DataFrame([input_data])
-
-    # Perform prediction
-    prediction = svm_model.predict(input_df)
-
-    # Display prediction result
-    st.write('## Prediction:')
-    st.write(f'The predicted severity category is: {prediction[0]}')
+# Membagi kolom
+col1, col2 = st.columns(2)
